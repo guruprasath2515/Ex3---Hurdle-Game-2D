061d681633b760f5fffea8728b3772ae15098722
guruprasath2515 <gurusivan2515@gmail.com>
2025-11-17 18:35:15 +0530

    Initial commit for Hurdle Game 2D

---
+++ Assets/Player.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+
+{
+    public float speed, jumpforce;
+    private Rigidbody2D rb;
+    public Score cc;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float moveinp = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(moveinp, 0, 0) * speed * Time.deltaTime;
+        if(Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(rb.linearVelocity.y) < 0.001f)
+        {
+            rb.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Destroy"))
+        {
+            cc.coincount++;
+            Destroy(other.gameObject);
+        }
+    }
+}
+
